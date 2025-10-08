bba7d45c01e2fbd3c70cd322d9d8a952b5f73165
ReemaAAsker <reemaaliasker@gmail.com>
2025-10-08 08:18:00 +0200

    payment uis

---
+++ android/app/src/main/kotlin/com/example/gas_station2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gas_station2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:gas_station2/screens/payment.dart';
+
+void main(List<String> args) {
+  runApp(APP());
+}
+
+class APP extends StatelessWidget {
+  APP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "Poppins",
+        primarySwatch: Colors.green,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontSize: 16),
+          bodyLarge: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CardDetailsScreen(),
+    );
+  }
+}
